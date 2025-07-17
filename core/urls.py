0dc63c29231c4d3ffd9354b66e62c5c81c777cbe
Akobir <akobirortiqov40@gmail.com>
2025-07-17 16:26:17 +0500

    assetsss fix

--- core/urls.py
+++ core/urls.py
@@ -18,9 +18,11 @@ from django.contrib import admin
 from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
+from pages.views import home_page_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', home_page_view, name='home'),
 ]
 
 if settings.DEBUG:
