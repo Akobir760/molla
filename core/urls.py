8070f4fe48fce595890fb8c54aada69aa41b7ac2
Akobir <akobirortiqov40@gmail.com>
2025-07-24 16:03:19 +0500

    write contact urls

--- core/urls.py
+++ core/urls.py
@@ -18,7 +18,6 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-from pages.views import home_page_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
