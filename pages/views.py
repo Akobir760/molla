51fd8b733944edb32d4fcbef921c42cd33829a0b
Akobir <akobirortiqov40@gmail.com>
2025-07-24 17:18:53 +0500

    fix contact_page_view

--- pages/views.py
+++ pages/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from django.contrib import messages
 
-# Create your views here.
+from pages.forms import ContactModelForm
+
 
 def home_page_view(request):
     return render(request, template_name='index-1.html')
@@ -8,7 +10,14 @@ def home_page_view(request):
 
 def contact_page_view(request):
     if request.method == "POST":
-        pass
-
+        form = ContactModelForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(
+                request, "Your message has been send to admins ✅"
+                )
+        else:
+            messages.error(request, "Please, check your data ❌")
+        return redirect('pages:contact')
     else:
         return render(request, 'pages/contact-2.html')
