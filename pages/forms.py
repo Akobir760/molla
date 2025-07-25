07bdebc9098de588bfc8623485b5507ea14bb447
Akobir <akobirortiqov40@gmail.com>
2025-07-25 17:35:31 +0500

    fix html

--- pages/forms.py
+++ pages/forms.py
@@ -6,3 +6,15 @@ class ContactModelForm(forms.ModelForm):
     class Meta:
         model = ContactModel
         fields = '__all__'
+
+
+    def clean_phone_number(self):
+        phone_number = self.cleaned_data.get('phone_number')
+        if not phone_number:
+            return phone_number
+        
+        if len(phone_number) != 13:
+            forms.ValidationError("Phone number should be exectly 13 length")
+        
+        return phone_number 
+
